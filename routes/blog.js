980102856d25a084389e1d65be3906814dfd3f85
Gary <xine9898@gmail.com>
2024-06-19 14:57:25 +0800

    測試資料庫能否連線

--- routes/blog.js
+++ routes/blog.js
@@ -5,6 +5,12 @@ import upload from './../utils/upload-img-blog.js';
 
 const router = express.Router();
 
+router.get("/", async (req,res) => {
+  const sql ="SELECT * from blog";
+  const [rows]= await db.query(sql);
+  res.json(rows)
+})
+
 router.post("/uploads",upload.array("photos",10),(req,res) => {
   res.json(req.files);
 })
