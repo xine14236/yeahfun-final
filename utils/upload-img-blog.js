e50c2319b30b4864021402ff0eb5225d1aa7abf1
Gary <xine9898@gmail.com>
2024-06-19 14:23:30 +0800

    新增上傳檔案

--- utils/upload-img-blog.js
+++ utils/upload-img-blog.js
@@ -1,5 +1,6 @@
 import multer from "multer";
 import {v4} from "uuid";
+import moment from "moment-timezone";
 
 //篩選檔案跟副檔名
 const extMap ={
@@ -20,9 +21,11 @@ const storage = multer.diskStorage({
 callback(null,"public/img-blog") //這裡的路徑和此JS檔所在的位置無關
   },
   filename: (req,file, callback)=>{
-    const timeName = `${Date.now()}`
+    const timeName = Date.now();
+   let timestampDate = moment(timeName);
+   let formattedDate = timestampDate.format('YYYYMMDDHHmmssSSS')
 const ext = extMap[file.mimetype]; // 取得副檔名
-callback(null, timeName+ ext);
+callback(null, formattedDate+ ext);
   },
 });
 
