33e6446c5d6b7dd87f302fed0f915c4476a8cca6
gary <xine9898@gmail.com>
2024-07-08 19:09:55 +0800

    重建前端

--- fontend/components/checkout/blocks/pay.js
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-
-export default function Pay() {
-  return <div>Pay</div>
-}
--- fontend/components/checkout/blocks/ship.js
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-
-export default function Ship() {
-  return <div>Ship</div>
-}
