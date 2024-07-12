4a85902bc4b2bc425f6c12f49f2c1cfcd03aa02c
Gary <xine9898@gmail.com>
2024-07-12 15:21:39 +0800

    安裝antd

---
+++ fontend/pages/antd/test.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import QuillToolbar from '@/components/blog/EditorToolbar'
+
+export default function test() {
+  return (
+    <div>
+      <QuillToolbar />
+    </div>
+  )
+}
