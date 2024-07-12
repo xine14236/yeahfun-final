338551c2f1cd91054e2bd658a7f05b024383b291
Yumi <nhb12980712@gmail.com>
2024-07-12 15:30:17 +0800

    更新models,seeds檔

--- backend/models/Store.js
+++ backend/models/Store.js
@@ -2,60 +2,52 @@ import { DataTypes } from 'sequelize'
 
 export default async function (sequelize) {
   return sequelize.define(
-    'Product',
+    'Store',
     {
-      id: {
+      stores_id: {
         type: DataTypes.INTEGER,
         primaryKey: true,
         autoIncrement: true,
       },
-      sn: {
-        type: DataTypes.STRING,
+      owners_id: {
+        type: DataTypes.INTEGER,
         allowNull: false,
       },
       name: {
         type: DataTypes.STRING,
-        allowNull: true,
-      },
-      brand_id: {
-        type: DataTypes.INTEGER,
         allowNull: false,
       },
-      cat_id: {
-        type: DataTypes.INTEGER,
+      mobile: {
+        type: DataTypes.STRING,
         allowNull: false,
       },
-      photos: {
-        type: DataTypes.TEXT,
-        allowNull: true,
+      address: {
+        type: DataTypes.STRING,
+        allowNull: false,
       },
-      stock: {
+      longitude: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      latitude: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      altitude: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      precautions: {
         type: DataTypes.STRING,
         allowNull: true,
       },
-      price: {
-        type: DataTypes.INTEGER,
-        allowNull: true,
-      },
-      info: {
-        type: DataTypes.TEXT,
-        allowNull: true,
-      },
-      color: {
-        type: DataTypes.STRING,
-        allowNull: true,
-      },
-      size: {
-        type: DataTypes.STRING,
-        allowNull: true,
-      },
-      tag: {
+      introduction: {
         type: DataTypes.STRING,
         allowNull: true,
       },
     },
     {
-      tableName: 'product', //直接提供資料表名稱
+      tableName: 'store', //直接提供資料表名稱
       timestamps: true, // 使用時間戳
       paranoid: false, // 軟性刪除
       underscored: true, // 所有自動建立欄位，使用snake_case命名
