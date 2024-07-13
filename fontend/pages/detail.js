a8379c967f825e5995870aa1ae8ecb47abb72636
Moza <moza13571025@gmail.com>
2024-07-13 15:24:20 +0800

    更新import各components的路徑

--- fontend/pages/detail.js
+++ fontend/pages/detail.js
@@ -1,9 +1,9 @@
 import React from 'react'
-import StoreTitleWrap from '@/components/product/storeTitleWrap'
-import StoreNormalInfo from '@/components/product/storeNormalInfo'
-import CampAreaSearchBar from '@/components/product/campAreaSearchBar'
-import CampAreasList from '@/components/product/campAreasList'
-import AttractionsNearby from '@/components/product/attractionsNearby'
+import StoreTitleWrap from '@/components/product//detail/storeTitleWrap'
+import StoreNormalInfo from '@/components/product/detail/storeNormalInfo'
+import CampAreaSearchBar from '@/components/product/detail/campAreaSearchBar'
+import CampAreasList from '@/components/product/detail/campAreasList'
+import AttractionsNearby from '@/components/product/detail/attractionsNearby'
 
 export default function Detail2() {
   return (
