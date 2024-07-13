cb82ad898d8e38b7b5f9f3a44ac65f6d8b7a712a
Moza <moza13571025@gmail.com>
2024-07-13 16:11:58 +0800

    detail路由能動態取得資料表stores單筆資料

--- backend/routes/detail.js
+++ backend/routes/detail.js
@@ -18,15 +18,17 @@ router.get('/', async function (req, res) {
   return res.json({ status: 'success', data: { store } })
 })
 
-// GET - 得到單筆資料(注意，有動態參數時要寫在GET區段最後面)
-// router.get('/:id', async function (req, res) {
-// 轉為數字
-// const id = Number(req.params.id)
+//GET - 得到單筆資料(注意，有動態參數時要寫在GET區段最後面)
+router.get('/:stores_id', async function (req, res) {
+  //轉為數字
+  const id = Number(req.params.stores_id)
 
-//   const [rows] = await db.query('SELECT * FROM stores WHERE stores_id = ?', [id])
-//   const product = rows[0]
+  const [rows] = await db.query('SELECT * FROM stores WHERE stores_id = ?', [
+    id,
+  ])
+  const store = rows[0]
 
-//   return res.json({ status: 'success', data: { product } })
-// })
+  return res.json({ status: 'success', data: { store } })
+})
 
 export default router
