d02b23f064914e062128eeffd947be0103b8474c
Yumi <nhb12980712@gmail.com>
2024-07-14 17:02:00 +0800

    icons

---
+++ fontend/components/icons/mail-logo.js
@@ -0,0 +1,5 @@
+import { FaEnvelope } from 'react-icons/fa'
+
+export default function MailLogo({ size, className }) {
+  return <FaEnvelope color="black" size={size} className={className} />
+}
---
+++ fontend/components/icons/opensuse.js
@@ -0,0 +1,5 @@
+import { FaOpensuse } from 'react-icons/fa6'
+
+export default function Opensuse({ color, size, className }) {
+  return <FaOpensuse color={color} size={size} className={className} />
+}
