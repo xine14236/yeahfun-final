59f72cc18f4c37399aa50b76412ee6002bc098c7
Yumi <nhb12980712@gmail.com>
2024-07-14 17:02:51 +0800

    default footer完成

--- fontend/components/layout/default-layout/my-footer.js
+++ fontend/components/layout/default-layout/my-footer.js
@@ -1,27 +1,68 @@
+import styles from '@/components/layout/default-layout/my-footer.module.scss'
+import Image from 'next/image'
+
+// icons
+import WhiteLogo from '@/components/icons/white-logo'
+import FacebookLogo from '@/components/icons/facebook-logo'
+import IgLogo from '@/components/icons/ig-logo'
+import MailLogo from '@/components/icons/mail-logo'
+
 export default function MyFooter() {
   return (
-    <footer className="footer mt-auto py-3 bg-light">
-      <div className="container">
-        <div className="text-muted">
-          <i className="bi bi-geo-alt-fill"></i> 台灣 © 2023 NextJS Inc.
-          版權所有
+    <footer className={styles.footer}>
+      <div className={`${styles.containerFluid} container-fluid`}>
+        <div className={styles.footerTop}>
+          <div className={styles.footerLink}>
+            <p>常見問題</p>
+            <ul>
+              <li>
+                <a href="#/">聯絡客服</a>
+              </li>
+              <li>
+                <a href="#/">關於我們</a>
+              </li>
+              <li>
+                <a href="#/">付款方式</a>
+              </li>
+              <li>
+                <a href="#/">取消訂單</a>
+              </li>
+            </ul>
+          </div>
+          <WhiteLogo width={110} className={styles.footerLogoWhite} />
+
+          <Image
+            className={styles.mountains}
+            src="../../../public/images/footer/mountains.png"
+            alt="mountains"
+            width={410}
+            height={95}
+          />
+          <Image
+            className={styles.frog}
+            src="../../../public/images/footer/frog.png"
+            alt="frog"
+            width={200}
+            height={225}
+          />
         </div>
-        <div className="text-muted">
-          <span className="pe-2">
-            本網站使用之圖片與媒體素材，均遵守相關授權使用規定。來源列表:
-          </span>
-          <a href="https://unsplash.com/" className="pe-2">
-            Unsplash
-          </a>
-          <a href="https://icons.getbootstrap.com/" className="pe-2">
-            Bootstrap Icons
-          </a>
-          <a href="https://commons.wikimedia.org/" className="pe-2">
-            Wikimedia Commons
-          </a>
-          <a href="https://fonts.google.com/icons" className="pe-2">
-            Material Icons
-          </a>
+        <hr />
+        <div className={styles.footerBottom}>
+          <div className={styles.footerPolicy}>
+            <div className={styles.policyText}>
+              <a href="#/">隱私政策</a>
+              <a href="#/">服務條款</a>
+              <a href="#/">Cookie 設定</a>
+            </div>
+          </div>
+          <div className={styles.socialField}>
+            <FacebookLogo size="25" className={styles.iconFB} />
+            <IgLogo size="25" className={styles.iconIg} />
+            <MailLogo size="25" className={styles.iconEmail} />
+          </div>
+          <div className={styles.footerCopyright}>
+            <p>© 2024 YeahFun 版權所有。</p>
+          </div>
         </div>
       </div>
     </footer>
