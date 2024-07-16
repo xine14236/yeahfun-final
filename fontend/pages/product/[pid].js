0d2a598629d4cd1f4899fdb3996c611774ade28c
Moza <moza13571025@gmail.com>
2024-07-16 13:44:15 +0800

    清除註解

--- fontend/pages/product/[pid].js
+++ fontend/pages/product/[pid].js
@@ -6,17 +6,9 @@ import StoreTitleWrap from '@/components/product//detail/storeTitleWrap'
 import StoreNormalInfo from '@/components/product/detail/storeNormalInfo'
 import CampAreaSearchBar from '@/components/product/detail/campAreaSearchBar'
 // import CampAreasList from '@/components/product/detail/campAreasList'
-// import StoreDetail from '@/components/product/storeDetail'
 // import AttractionsNearby from '@/components/product/detail/attractionsNearby'
 
 export default function Detail() {
-  // const router = useRouter()
-  // const { pid } = router.query
-
-  // const [people, setPeople] = useState(1)
-
-  // const peopleOptions = new Array(12).fill().map((_, i) => i + 1)
-
   return (
     <>
       <StoreTitleWrap />
