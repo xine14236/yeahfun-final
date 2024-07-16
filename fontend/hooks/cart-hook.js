cb027cd857b225f3020efe2a1864d545bc308c37
ChiaChun1997 <alexchow297@gmail.com>
2024-07-16 10:47:28 +0800

    前端購物車鉤子測試

--- fontend/hooks/cart-hook.js
+++ fontend/hooks/cart-hook.js
@@ -1,20 +1,27 @@
-import React, { createContext, useState, useContext } from 'react';
+import { useState, useContext, createContext } from 'react'
 
-// 创建一个 Context
-const CartContext = createContext();
+// 創建一个 Context 用于共享購物車數據
+const CartContext = createContext()
 
-// 创建一个 Provider 组件
-export function CartProviderNew({ children }) {
-  const [cartItems, setCartItems] = useState([]);
-
-  return (
-    <CartContext.Provider value={{ cartItems, setCartItems }}>
-      {children}
-    </CartContext.Provider>
-  );
+export const useCart = () => {
+  return useContext(CartContext)
 }
 
-// 创建一个自定义 Hook 方便使用 CartContext
-export function useCart() {
-  return useContext(CartContext);
+export const CartProviderNew = ({ children }) => {
+  const [cart, setCart] = useState([])
+
+  const addToCart = (store) => {
+    setCart((prevCart) => [...prevCart, store])
+  }
+  const removeFromCart = (id) => {
+    setCart((prevCart) => prevCart.filter((item) => item.id !== id))
+  }
+
+  const value = {
+    cart,
+    addToCart,
+    removeFromCart,
+  }
+
+  return <CartContext.Provider value={value}>{children}</CartContext.Provider>
 }
