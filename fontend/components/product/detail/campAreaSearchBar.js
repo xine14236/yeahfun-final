98480a9db6f5e221cd09af05f59c45f642d92ab8
Moza <moza13571025@gmail.com>
2024-07-16 13:40:27 +0800

    campAreaSearchBar.js(選擇日期人數篩選房型)元件化

--- fontend/components/product/detail/campAreaSearchBar.js
+++ fontend/components/product/detail/campAreaSearchBar.js
@@ -1,44 +1,89 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useRouter } from 'next/router'
+import StoreDetail from '@/components/product/storeDetail'
 
 export default function CampAreaSearchBar() {
+  const router = useRouter()
+  const { pid } = router.query
+
+  const [people, setPeople] = useState(1)
+
+  const peopleOptions = new Array(12).fill().map((_, i) => i + 1)
+
   return (
     <>
-              <div className="campAreaSearchBar" style={{ border: '1px solid red' }}>
-          <form className="inputDateAndNumber" action="">
-            <div className="inputDate">
-              <div
-                htmlFor=""
-                style={{
-                  color: 'var(--white, /FFF)',
-                  fontFamily: 'Inter',
-                  fontSize: 18,
-                  fontStyle: 'normal',
-                  fontWeight: 500,
-                  lineHeight: 'normal',
-                }}
-              >
-                請選擇入住日期：
-              </div>
-              <input type="text" />
+      {/* 輸入日期、人數 */}
+      <div className="campAreaSearchBar" style={{ border: '1px solid red' }}>
+        <form className="inputDateAndNumber" action="">
+          <div className="inputDate">
+            <div
+              htmlFor=""
+              style={{
+                color: 'var(--white, /FFF)',
+                fontFamily: 'Inter',
+                fontSize: 18,
+                fontStyle: 'normal',
+                fontWeight: 500,
+                lineHeight: 'normal',
+              }}
+            >
+              請選擇入住日期：
             </div>
-            <div className="inputNumber">
-              <span
-                htmlFor=""
-                style={{
-                  color: 'var(--white, /FFF)',
-                  fontFamily: 'Inter',
-                  fontSize: 18,
-                  fontStyle: 'normal',
-                  fontWeight: 500,
-                  lineHeight: 'normal',
-                }}
-              >
-                請選擇入住人數：
-              </span>
-              <input type="text" />
-            </div>
-          </form>
-        </div>
+            <input type="text" />
+          </div>
+
+          <div className="inputNumber">
+            入住人數
+            <select
+              htmlFor=""
+              style={
+                {
+                  // color: 'var(--white, #FFF)',
+                  // fontFamily: 'Inter',
+                  // fontSize: 18,
+                  // fontStyle: 'normal',
+                  // fontWeight: 500,
+                  // lineHeight: 'normal',
+                }
+              }
+              value={people}
+              onChange={(e) => {
+                setPeople(e.target.value)
+              }}
+            >
+              {/* 為了要對應初始的city狀態，加入這個初始或未選擇的選項 */}
+              <option value="">請選擇人數</option>
+              {peopleOptions.map((v, i) => {
+                return (
+                  <option key={i} value={v}>
+                    {v}人以上
+                  </option>
+                )
+              })}
+            </select>
+          </div>
+        </form>
+      </div>
+
+      {/* pid && ( ... )：条件判断，避免在 pid 不存在时出现渲染错误或异常情况。 */}
+      {pid && (
+        <>
+          <div>
+            <StoreDetail
+              title="房型選擇"
+              type="bed"
+              pid={pid}
+              people={people}
+            />
+            <StoreDetail
+              title="營區選擇"
+              type="tent"
+              pid={pid}
+              people={people}
+            />
+          </div>
+        </>
+      )}
     </>
   )
 }
