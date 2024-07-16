3879ae1b24101999ba638365bd58edef4b9291ed
Gary <xine9898@gmail.com>
2024-07-16 13:03:38 +0800

    建立modal

--- fontend/pages/blog.js
+++ fontend/pages/blog.js
@@ -1,13 +1,22 @@
 import React, { useEffect, useState } from 'react'
+import dynamic from 'next/dynamic';
 import styles from '@/styles/blog.module.css'
 import heart from '@/assets/heart.svg'
 import Image from 'next/image'
 import { FaSearch } from 'react-icons/fa'
-import { DatePicker } from 'antd';
-const { RangePicker } = DatePicker;
+import { DatePicker, Space,Modal } from 'antd'
+import blogCategory from '@/data/blog/BlogCategory.json'
+const { RangePicker } = DatePicker
+const BlogCategoryModal = dynamic(() => import('@/components/blog/blogCategoryModal'), {
+  ssr: false,
+});
+
 
 export default function Blog() {
-
+  const initialCate = blogCategory.map((v, i) => {
+    return { ...v, check: false }
+  })
+  console.log(initialCate)
 
   const [blogs, setBlogs] = useState([])
   const [total, setTotal] = useState(0) //總筆數
@@ -15,20 +24,24 @@ export default function Blog() {
   const [page, setPage] = useState(1)
   const [perpage, setPerPage] = useState(10)
 
-   // 排序
-   const [sort, setSort] = useState('id')
-   const [order, setOrder] = useState('asc')
+  // 排序
+  const [sort, setSort] = useState('id')
+  const [order, setOrder] = useState('asc')
 
-   const [selectedRange, setSelectedRange] = useState([null, null]);
+  const [selectedRange, setSelectedRange] = useState([null, null])
 
-   const handleRangeChange = (dates, dateStrings) => {
-    console.log('Selected Range:', dates);
-    setSelectedRange(dates); // 更新状态变量
-    if(dates){
+  const [category, setCategory] = useState(initialCate)
 
+  const [visible, setVisible] = useState(false);
+
+
+  const handleRangeChange = (dates, dateStrings) => {
+    console.log('Selected Range:', dates)
+    setSelectedRange(dates) // 更新状态变量
+    if (dates) {
       console.log(dates[0].$d)
     }
-  };
+  }
 
   const getLists = async (params = {}) => {
     const baseUrl = 'http://localhost:3005/api/blog'
@@ -41,16 +54,14 @@ export default function Blog() {
     try {
       const res = await fetch(url)
       const resData = await res.json()
-console.log(resData)
+      console.log(resData)
 
       if (resData.success === true) {
         setPageCount(resData.data.pageCount)
         setTotal(resData.data.total)
         if (Array.isArray(resData.data.blogs)) {
-         
           setBlogs(resData.data.blogs)
         }
-        
       }
 
       // 設定到狀態中 ===>進入update階段，觸發重新渲染(re-render)，呈現資料
@@ -60,12 +71,20 @@ console.log(resData)
     }
   }
   const stripHtmlTags = (str) => {
-    return str.replace(/<[^>]*>/g, '');  // 使用正則表達式去除所有 HTML 標籤
+    return str.replace(/<[^>]*>/g, '') // 使用正則表達式去除所有 HTML 標籤
+  }
+
+  const showModal = () => {
+    setVisible(true);
   };
 
+  const handleOk = () => {
+    setVisible(false);
+  };
 
-
-
+  const handleCancel = () => {
+    setVisible(false);
+  };
 
   useEffect(() => {
     //  建立搜尋參數物件
@@ -77,134 +96,138 @@ console.log(resData)
     }
     // 向伺服器fetch
     getLists(params)
-  }, [ page,
-    perpage,
-    sort,
-    order,])
+  }, [page, perpage, sort, order])
 
-
-  
   return (
     <>
       <div className="container">
-     
         <div id="blogFilter">
           <div className="row  ">
             <div
-              className={`col-12 col-sm-4 col-lg-5 border d-flex flex-column  ${styles.filterControl1}`}
-              style={{ height: 100 }}
+              className={`col-12 col-sm-4 col-lg-5 border d-flex flex-column  ${styles.filterControl1} ${styles.h150}`}
             >
-            <div className="row">
-            <div className="col-md-6 col-12 d-flex align-items-center">
+              <div className="row">
+                <div className="col-lg-6 col-12 d-flex align-items-center">
+                  <select className="form-select ">
+                    <option value="id,asc">依id排序(由小至大)</option>
+                    <option value="id,desc">依id排序(由大至小)</option>
+                    <option value="author,asc">依作者排序(由低至高)</option>
+                    <option value="author,desc">依作者排序(由高至低)</option>
+                  </select>
+                </div>
 
-            <select className='form-select '>
-          <option value="id,asc">依id排序(由小至大)</option>
-          <option value="id,desc">依id排序(由大至小)</option>
-          <option value="price,asc">依價格排序(由低至高)</option>
-          <option value="price,desc">依價格排序(由高至低)</option>
-        </select>
-            </div>
-       
-
-
-           <div className="col-md-6 col-12">
-           <input
-                type="text"
-                className={`form-control  ${styles.filterWidth1}`}
-                placeholder=""
-              />
-              <div className="form-text text-center">
-                透過部落格標題搜尋 <FaSearch />
+                <div className="col-lg-6 col-12 ">
+                  <input
+                    type="text"
+                    className={`form-control  ${styles.filterWidth1}`}
+                    placeholder=""
+                  />
+                  <span
+                    className={`form-text text-center ${styles.marginInline}`}
+                  >
+                    透過部落格標題搜尋 <FaSearch />
+                  </span>
+                </div>
               </div>
-           </div>
-            </div>
-             
             </div>
             <div
-              className="col-12 col-sm-6  col-lg-5 border d-flex justify-content-evenly align-items-center"
-              style={{ height: 100 }}
+              className={`col-12 col-sm-6  col-lg-5 border d-flex justify-content-evenly align-items-center ${styles.h150}`}
             >
-               <RangePicker showTime  onChange={handleRangeChange}/>
+         
+         <Space direction="vertical">
+
+                <RangePicker showTime onChange={handleRangeChange} />
+         <div className={`form-text text-center `}>
+                透過部落格時間搜尋 <FaSearch />
+              </div>
+               
+         </Space>
+
+          
             </div>
             <div
-              className="col-12 col-sm-2 col-lg  border d-flex  align-items-center justify-content-sm-center "
-              style={{ height: 100 }}
+              className={`col-12 col-sm-2 col-lg  border d-flex  align-items-center justify-content-sm-center ${styles.h150}`}
             >
               <FaSearch />
             </div>
+            <div className={`col-12 border ${styles.blogcategory}`} onClick={showModal} >點我展開分類搜尋</div>
           </div>
+
         </div>
         <div className="row" id="card-container"></div>
       </div>
       <div className="container">
-        {
-          blogs.map((v, i) => {
-            return (
-              <div className="row " id="card-container" key={v.id}>
-                {/* 卡片内容会在这里动态生成 */}
-                <div
-                  className={`card col-md-12 ${styles.cardWrapper} col-lg-9 ${
-                    i % 2 === 1 ? `${styles.cardRight}` : `${styles.cardLeft}`
-                  }`}
-                >
-                  <div className="row g-0 ">
-                    <div
-                      className={`col-md-5 d-flex flex-column my-auto ${
-                        i % 2 === 1 ? `${styles.order1}` : ''
-                      } `}
-                    >
-                      <Image
-                        src='http://localhost:3005/img-blog/2e0910f14f50dfb9901999ab4dcb50db.webp'
-                        className="img-fluid"
-                        alt="..."
-                        width={400}
-                        height={350}
-                        style={{ width: '100%', height: 'auto',minHeight:'280px', objectFit: 'cover' }}
-                      />
-                    </div>
-                    <div className="col-md-7 d-flex flex-column">
-                      <div className={`card-body ${styles.order0}`}>
-                        <div className="d-flex flex-column">
-                          <h3
-                            className={`card-title mt-md-3 ${styles.color1} ${styles.textTruncate2}`}
-                          >
-                           {v.title}
-                          </h3>
-                          <div className="d-flex justify-content-between ">
-                            <p className="card-text ">
-                              <small className="text-muted">
-                               { v.date}
-                              </small>
-                            </p>
-                            <div>
-                              <Image src={heart} />
-                              <span className="ms-3">{v.likes_count}</span>
-                            </div>
-                          </div>
-                          <p className="card-text text-muted mb-md-5">
-                            <small className="text-muted">{v.author}</small>
+        {blogs.map((v, i) => {
+          return (
+            <div className="row " id="card-container" key={v.id}>
+              {/* 卡片内容会在这里动态生成 */}
+              <div
+                className={`card col-md-12 ${styles.cardWrapper} col-lg-9 ${
+                  i % 2 === 1 ? `${styles.cardRight}` : `${styles.cardLeft}`
+                }`}
+              >
+                <div className="row g-0 ">
+                  <div
+                    className={`col-md-5 d-flex flex-column my-auto ${
+                      i % 2 === 1 ? `${styles.order1}` : ''
+                    } `}
+                  >
+                    <Image
+                      src="http://localhost:3005/img-blog/2e0910f14f50dfb9901999ab4dcb50db.webp"
+                      className="img-fluid"
+                      alt="..."
+                      width={400}
+                      height={350}
+                      style={{
+                        width: '100%',
+                        height: 'auto',
+                        minHeight: '280px',
+                        objectFit: 'cover',
+                      }}
+                    />
+                  </div>
+                  <div className="col-md-7 d-flex flex-column">
+                    <div className={`card-body ${styles.order0}`}>
+                      <div className="d-flex flex-column">
+                        <h3
+                          className={`card-title mt-md-3 ${styles.color1} ${styles.textTruncate2}`}
+                        >
+                          {v.title}
+                        </h3>
+                        <div className="d-flex justify-content-between ">
+                          <p className="card-text ">
+                            <small className="text-muted">{v.date}</small>
                           </p>
-                          <div
-                            className={`card-content mb-md-3 ${styles.textTruncate4}`}
-                          >
-                            <h5 className="card-text">
-                            { stripHtmlTags(v.content).replace(/\\r\\n/g, '').replace(/\s+/g, '')}
-                            </h5>
+                          <div>
+                            <Image src={heart} />
+                            <span className="ms-3">{v.likes_count}</span>
                           </div>
                         </div>
-                        <a href="" className="text-decoration-none text-end ">
-                          <p className={`card-text ${styles.color2}`}>
-                            查看更多
-                          </p>
-                        </a>
+                        <p className="card-text text-muted mb-md-5">
+                          <small className="text-muted">{v.author}</small>
+                        </p>
+                        <div
+                          className={`card-content mb-md-3 ${styles.textTruncate4}`}
+                        >
+                          <h5 className="card-text">
+                            {stripHtmlTags(v.content)
+                              .replace(/\\r\\n/g, '')
+                              .replace(/\s+/g, '')}
+                          </h5>
+                        </div>
                       </div>
+                      <a href="" className="text-decoration-none text-end ">
+                        <p className={`card-text ${styles.color2}`}>查看更多</p>
+                      </a>
                     </div>
                   </div>
                 </div>
               </div>
-            )
-          })}
+            </div>
+          )
+        })}
       </div>
+      <BlogCategoryModal visible={visible} handleOk={handleOk} handleCancel={handleCancel} />
     </>
   )
 }
