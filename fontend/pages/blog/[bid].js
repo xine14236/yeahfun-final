89803977b52f6cfdeb90b32d827289d1fd213d47
Gary <xine9898@gmail.com>
2024-07-17 14:04:22 +0800

    跑馬燈

--- fontend/pages/blog/[bid].js
+++ fontend/pages/blog/[bid].js
@@ -1,11 +1,14 @@
 import React from 'react'
+import Carousel from '@/components/blog/carousel'
 
 export default function blogDetail() {
   return (
     <>
-      <div className="row"></div>
       <div className="row">
-        <div className="col-12 col-md-9 border"></div>
+      <Carousel/>
+      </div>
+      <div className="row">
+        <div className="col-12 col-md-9 border" ></div>
         <div className="col-12 col-md-3 border"></div>
       </div>
     </>
