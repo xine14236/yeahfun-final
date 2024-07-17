df6466bc0074f0181f3835e8e78248e84d2d1eb7
Gary <xine9898@gmail.com>
2024-07-17 11:48:51 +0800

    建立詳細頁

---
+++ fontend/pages/blog/[bid].js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function blogDetail() {
+  return (
+    <>
+      <div className="row"></div>
+      <div className="row">
+        <div className="col-12 col-md-9 border"></div>
+        <div className="col-12 col-md-3 border"></div>
+      </div>
+    </>
+  )
+}
