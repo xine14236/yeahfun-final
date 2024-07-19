24791fedf99dc5bc0c3a42bb38cc629d148f3ddb
Gary <xine9898@gmail.com>
2024-07-19 15:33:57 +0800

    上傳成功

--- fontend/components/blog/test.js
+++ fontend/components/blog/test.js
@@ -1,11 +1,63 @@
-import React, { useState } from 'react'
+import React, { useCallback, useRef, useState } from 'react'
 import ReactQuill from 'react-quill'
 // import 'react-quill/dist/quill.bubble.css'
 import 'react-quill/dist/quill.snow.css'
 
 export default function MyComponent() {
   const [value, setValue] = useState('')
+  const reactQuillRef = useRef(null);
+
+  const uploadToCloudinary = async (file)=> {
+    const formData = new FormData();
+    formData.append("photos", file);
+   
+    const res = await fetch(
+      `http://localhost:3005/api/blog/uploads/23`,
+      { method: "POST", body: formData }
+    );
+    const data = await res.json();
+    const url = 'http://localhost:3005/img-blog/'+data.data[0].filename;
+  
+    return url
+  }
+
+  const imageHandler = useCallback(() => {
+    const input = document.createElement("input");
+    input.setAttribute("type", "file");
+    input.setAttribute("accept", "image/*");
+    input.click();
+    input.onchange = async () => {
+      if (input !== null && input.files !== null) {
+        const file = input.files[0];
+        const url = await uploadToCloudinary(file);
+        const quill = reactQuillRef.current;
+        if (quill) {
+          const range = quill.getEditorSelection();
+          range && quill.getEditor().insertEmbed(range.index, "image", url);
+        }
+      }
+    };
+  }, []);
+
+  const modules = {
+    toolbar: {
+          container: [
+            ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
+            ['blockquote', 'code-block'],
+            ['link', 'image'],
+            [{ 'header': [ 2, 3, 4, 5, 6, false] }],
+            [{ 'color': [] }, { 'background': [] }],
+            [{ 'align': [] }],
+
+            ['clean']   
+         
+          ],
+          handlers: {
+            image: imageHandler,   // <- 
+          },
+        },
+  };
 
   // theme="bubble"
-  return <ReactQuill theme="snow" value={value} onChange={setValue} />
+  return <ReactQuill ref={reactQuillRef} theme="snow" value={value} onChange={setValue} modules={modules} />
 }
