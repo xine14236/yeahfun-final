3eba5365136f7e25e27a82543bdc213f22a2a719
Gary <xine9898@gmail.com>
2024-07-22 09:56:16 +0800

    add葉面

--- fontend/pages/blog/add/[bcid].js
+++ fontend/pages/blog/add/[bcid].js
@@ -1,6 +1,10 @@
 import dynamic from 'next/dynamic'
 import { useEffect, useState } from 'react'
 import { useRouter } from 'next/router';
+import blogCategory from '@/data/blog/BlogCategory.json'
+import { Select, Button, Tag } from 'antd';
+import Swal from 'sweetalert2'
+import withReactContent from 'sweetalert2-react-content'
 
 
 const MyComponent = dynamic(() => import('@/components/blog/test'), {
@@ -14,8 +18,45 @@ export default function Test() {
     const router = useRouter();
     const [value, setValue] = useState('')
     const [blogId,setBlogId]=useState(0)
+    const initialCate = blogCategory.filter((v)=>v.id>7)
+    const [tags, setTags] = useState(initialCate);
+    const [selectedTags, setSelectedTags] = useState([]);
+    const [title, setTitle]=useState('')
+    const MySwal = withReactContent(Swal)
+
+    const handleAddTag = (value) => {
+      const tag = tags.find(t => t.id === value);
+      if (tag && !selectedTags.some(t => t.id === tag.id)) {
+        setSelectedTags([...selectedTags, tag]);
+      }
+    };
+  
+    const handleRemoveTag = (id) => {
+      setSelectedTags(selectedTags.filter(tag => tag.id !== id));
+    };
+  
+    const handleSubmit = async () => {
+      const payload = {
+        blogId,
+        title,
+        content: value,
+        tags: selectedTags.map(tag => tag.id),
+      };
+  
+      console.log(payload)
+      await fetch('/api/blog/save', {
+        method: 'POST',
+        credentials: 'include', 
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(payload),
+      });
+    };
+
 
     useEffect(() => {
+     
     
 
         if (router.isReady) {
@@ -26,6 +67,51 @@ export default function Test() {
         // 以下為注解掉eslint的警告一行
         // eslint-disable-next-line
       }, [router.isReady])
+   
     
-  return <MyComponent value={value} setValue={setValue} blogId={blogId}  />
+  return(  <>
+  <div className="my-5">
+  <label htmlFor="">
+  <h3 className='mb-3'>Title:</h3>
+  <input type="text" className='form-control' value={title} onChange={(e)=>{setTitle(e.target.value)}}/>
+  </label>
+ 
+
+
+  </div>
+    <div className='my-5'>
+         <h3 className='mb-3'>Tags:</h3>
+         <Select
+           style={{ width: 200 }}
+           placeholder="Select a tag"
+           onChange={handleAddTag}
+           value={undefined}
+         >
+           {tags.map((tag) => (
+             <Option key={tag.id} value={tag.id}>
+               {tag.blog_category_name}
+             </Option>
+           ))}
+         </Select>
+         <div style={{ marginTop: 16 }}>
+           {selectedTags.map((tag) => (
+             <Tag
+               key={tag.id}
+               closable
+               onClose={() => handleRemoveTag(tag.id)}
+             >
+               {tag.blog_category_name}
+             </Tag>
+           ))}
+         </div>
+       </div>
+       <div className="my-5">
+       <MyComponent value={value} setValue={setValue} blogId={blogId}  />
+       </div>
+       <Button type="primary" onClick={handleSubmit} style={{ marginTop: 16 }}>
+        Submit
+      </Button>
+   </>)
+ 
+
 }
