58a457960999e49ef7e154ecd9d6318a643b8418
leechinghua <leechenghua107@gmail.com>
2024-07-24 22:25:38 +0800

    更改google第三方登入後端路由

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -10,7 +10,7 @@ import { getIdParam } from '#db-helpers/db-tool.js'
 // 資料庫使用
 import { Op } from 'sequelize'
 import sequelize from '#configs/db.js'
-const { User } = sequelize.models
+const { Customer } = sequelize.models
 
 // 驗証加密密碼字串用
 import { compareHash } from '#db-helpers/password-hash.js'
@@ -27,7 +27,7 @@ const storage = multer.diskStorage({
   },
   filename: function (req, file, callback) {
     // 經授權後，req.user帶有會員的id
-    const newFilename = req.user.id
+    const newFilename = req.customer.id
     // 新檔名由表單傳來的req.body.newFilename決定
     callback(null, newFilename + path.extname(file.originalname))
   },
@@ -37,11 +37,11 @@ const upload = multer({ storage: storage })
 
 // GET - 得到所有會員資料
 router.get('/', async function (req, res) {
-  const users = await User.findAll({ logging: console.log })
+  const customer = await Customer.findAll({ logging: console.log })
   // 處理如果沒找到資料
 
   // 標準回傳JSON
-  return res.json({ status: 'success', data: { users } })
+  return res.json({ status: 'success', data: { customer } })
 })
 
 // GET - 得到單筆資料(注意，有動態參數時要寫在GET區段最後面)
@@ -50,18 +50,18 @@ router.get('/:id', authenticate, async function (req, res) {
   const id = getIdParam(req)
 
   // 檢查是否為授權會員，只有授權會員可以存取自己的資料
-  if (req.user.id !== id) {
+  if (req.customer.id !== id) {
     return res.json({ status: 'error', message: '存取會員資料失敗' })
   }
 
-  const user = await User.findByPk(id, {
+  const customer = await Customer.findByPk(id, {
     raw: true, // 只需要資料表中資料
   })
 
   // 不回傳密碼
-  delete user.password
+  delete customer.password
 
-  return res.json({ status: 'success', data: { user } })
+  return res.json({ status: 'success', data: { customer } })
 })
 
 // POST - 新增會員資料
@@ -75,14 +75,14 @@ router.post('/', async function (req, res) {
   // }
 
   // 要新增的會員資料
-  const newUser = req.body
+  const newCustomer = req.body
 
   // 檢查從前端來的資料哪些為必要(name, username...)
   if (
-    !newUser.username ||
-    !newUser.email ||
-    !newUser.name ||
-    !newUser.password
+    !newCustomer.username ||
+    !newCustomer.email ||
+    !newCustomer.name ||
+    !newCustomer.password
   ) {
     return res.json({ status: 'error', message: '缺少必要資料' })
   }
@@ -90,15 +90,18 @@ router.post('/', async function (req, res) {
   // 執行後user是建立的會員資料，created為布林值
   // where指的是不可以有相同的資料，如username或是email不能有相同的
   // defaults用於建立新資料用需要的資料
-  const [user, created] = await User.findOrCreate({
+  const [customer, created] = await Customer.findOrCreate({
     where: {
-      [Op.or]: [{ username: newUser.username }, { email: newUser.email }],
+      [Op.or]: [
+        { username: newCustomer.username },
+        { email: newCustomer.email },
+      ],
     },
     defaults: {
-      name: newUser.name,
-      password: newUser.password,
-      username: newUser.username,
-      email: newUser.email,
+      name: newCustomer.name,
+      password: newCustomer.password,
+      username: newCustomer.username,
+      email: newCustomer.email,
     },
   })
 
@@ -128,11 +131,11 @@ router.post(
     // console.log(req.file, req.body)
 
     if (req.file) {
-      const id = req.user.id
+      const id = req.customer.id
       const data = { avatar: req.file.filename }
 
       // 對資料庫執行update
-      const [affectedRows] = await User.update(data, {
+      const [affectedRows] = await Customer.update(data, {
         where: {
           id,
         },
@@ -161,7 +164,7 @@ router.put('/:id/password', authenticate, async function (req, res) {
   const id = getIdParam(req)
 
   // 檢查是否為授權會員，只有授權會員可以存取自己的資料
-  if (req.user.id !== id) {
+  if (req.customer.id !== id) {
     return res.json({ status: 'error', message: '存取會員資料失敗' })
   }
 
@@ -178,7 +181,7 @@ router.put('/:id/password', authenticate, async function (req, res) {
   }
 
   // 查詢資料庫目前的資料
-  const dbUser = await User.findByPk(id, {
+  const dbUser = await Customer.findByPk(id, {
     raw: true, // 只需要資料表中資料
   })
 
@@ -197,7 +200,7 @@ router.put('/:id/password', authenticate, async function (req, res) {
   }
 
   // 對資料庫執行update
-  const [affectedRows] = await User.update(
+  const [affectedRows] = await Customer.update(
     { password: userPassword.new },
     {
       where: {
@@ -221,20 +224,20 @@ router.put('/:id/profile', authenticate, async function (req, res) {
   const id = getIdParam(req)
 
   // 檢查是否為授權會員，只有授權會員可以存取自己的資料
-  if (req.user.id !== id) {
+  if (req.customer.id !== id) {
     return res.json({ status: 'error', message: '存取會員資料失敗' })
   }
 
   // user為來自前端的會員資料(準備要修改的資料)
-  const user = req.body
+  const customer = req.body
 
   // 檢查從前端瀏覽器來的資料，哪些為必要(name, ...)
-  if (!id || !user.name) {
+  if (!id || !customer.name) {
     return res.json({ status: 'error', message: '缺少必要資料' })
   }
 
   // 查詢資料庫目前的資料
-  const dbUser = await User.findByPk(id, {
+  const dbUser = await Customer.findByPk(id, {
     raw: true, // 只需要資料表中資料
   })
 
@@ -244,12 +247,12 @@ router.put('/:id/profile', authenticate, async function (req, res) {
   }
 
   // 有些特殊欄位的值沒有時要略過更新，不然會造成資料庫錯誤
-  if (!user.birth_date) {
-    delete user.birth_date
+  if (!customer.birth_date) {
+    delete customer.birth_date
   }
 
   // 對資料庫執行update
-  const [affectedRows] = await User.update(user, {
+  const [affectedRows] = await Customer.update(customer, {
     where: {
       id,
     },
@@ -261,7 +264,7 @@ router.put('/:id/profile', authenticate, async function (req, res) {
   }
 
   // 更新成功後，找出更新的資料，updatedUser為更新後的會員資料
-  const updatedUser = await User.findByPk(id, {
+  const updatedUser = await Customer.findByPk(id, {
     raw: true, // 只需要資料表中資料
   })
 
@@ -276,7 +279,7 @@ router.put('/:id/profile', authenticate, async function (req, res) {
 router.delete('/:id', async function (req, res) {
   const id = getIdParam(req)
 
-  const affectedRows = await User.destroy({
+  const affectedRows = await Customer.destroy({
     where: {
       id,
     },
