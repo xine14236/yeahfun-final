9741bea5f4d3a51fa4593fe4d384c71bda5e3a18
Yumi <nhb12980712@gmail.com>
2024-07-25 23:14:55 +0800

    navbar

--- fontend/components/layout/default-layout/my-navbar/member-bar.js
+++ fontend/components/layout/default-layout/my-navbar/member-bar.js
@@ -26,55 +26,65 @@ export default function MemberBar() {
         // className="nav-item dropdown"
         className={` dropdown ${styles['dropdown']}`}
       >
-        <Link
-          className={`dropdown-toggle btn ${styles.DropdownToggle}`}
-          href="/customer"
-          role="button"
-          data-bs-toggle="dropdown"
-          aria-expanded="false"
-          title="會員中心"
-        >
-          <h5 className={` ${styles.member}`}>
-            <Opensuse size={70} className={styles.iconOpensuse} />
-            會員中心
-          </h5>
-          <p className="d-none d-md-inline d-lg-none">會員中心</p>
-        </Link>
-        <ul
-          className={`dropdown-menu dropdown-menu-end p-4  ${styles['slideIn']} ${styles['dropdown-menu']}`}
-        >
-          <li>
-            <h6 className="text-center">會員中心</h6>
-            <p className="text-center">
-              會員姓名: {userName}
-              <br />
-              帳號: {userMail}
-            </p>
-          </li>
-          <li>
-            <hr className="dropdown-divider" />
-          </li>
-          <li>
-            <Link className="dropdown-item text-center" href="/customer">
-              會員管理區
-            </Link>
-          </li>
-
-          <li>
-            <Link className="dropdown-item text-center " href="/about">
-              兌換
-            </Link>
-          </li>
-          <li>
+        {auth.userData.id ? (
+          <>
             <Link
-              href="#/"
-              onClick={handleLogout}
-              className="dropdown-item text-center "
+              className={`dropdown-toggle btn ${styles.DropdownToggle}`}
+              href="/customer"
+              role="button"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+              title="會員中心"
             >
-              登出
+              <h5 className={` ${styles.member}`}>
+                <Opensuse size={70} className={styles.iconOpensuse} />
+                會員中心
+              </h5>
+              <p className="d-none d-md-inline d-lg-none">會員中心</p>
+            </Link>
+            <ul
+              className={`dropdown-menu dropdown-menu-end p-4  ${styles['slideIn']} ${styles['dropdown-menu']}`}
+            >
+              <li>
+                <h6 className="text-center">會員中心</h6>
+                <p className="text-center">
+                  會員姓名: {userName}
+                  <br />
+                  帳號: {userMail}
+                </p>
+              </li>
+              <li>
+                <hr className="dropdown-divider" />
+              </li>
+              <li>
+                <Link className="dropdown-item text-center" href="/customer">
+                  會員管理區
+                </Link>
+              </li>
+
+              <li>
+                <Link className="dropdown-item text-center " href="/about">
+                  兌換
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="#/"
+                  onClick={handleLogout}
+                  className="dropdown-item text-center "
+                >
+                  登出
+                </Link>
+              </li>
+            </ul>
+          </>
+        ) : (
+          <li>
+            <Link href="/welcome/login" className={`btnOrangePc transition`}>
+              <div>登入/註冊</div>
             </Link>
           </li>
-        </ul>
+        )}
       </li>
       <li>
         {/* <button className={`btnOrangePc transition`}>登入/註冊</button> */}
--- fontend/components/layout/list-layout/list-navbar/list-member-bar.js
+++ fontend/components/layout/list-layout/list-navbar/list-member-bar.js
@@ -4,71 +4,96 @@ import styles from '@/components/layout/list-layout/list-navbar/list-member-bar.
 import ShoppingBag from '@/components/icons/shopping-bag'
 import Opensuse from '@/components/icons/opensuse'
 import { useAuth } from '@/hooks/use-auth'
+//購物車有商品時顯示紅點
+import { useState, useEffect } from 'react'
+import { useCart } from '@/hooks/cart-hook'
 
-export default function ListMemberBar() {
+export default function MemberBar() {
   const { auth, handleLogout } = useAuth()
+  //購物車有商品時顯示紅點
+  const [showRedDot, setShowRedDot] = useState(false)
+  const { addCart, cartItems } = useCart()
   const userName = auth.userData.name
   const userMail = auth.userData.email
-  return (
-    <>
-      <ul className="navbar-nav pe-2 ms-auto d-flex gap-4 align-items-center">
-        <li className={` dropdown ${styles['dropdown']}`}>
-          <Link
-            className={`dropdown-toggle btn ${styles.DropdownToggle}`}
-            href="/customer"
-            role="button"
-            data-bs-toggle="dropdown"
-            aria-expanded="false"
-            title="會員中心"
-          >
-            <h5 className={` ${styles.member}`}>
-              <Opensuse size={70} className={styles.iconOpensuse} />
-              會員中心
-            </h5>
-            <p className="d-none d-md-inline d-lg-none">會員中心</p>
-          </Link>
-          <ul
-            className={`dropdown-menu dropdown-menu-end p-4  ${styles['slideIn']} ${styles['dropdown-menu']}`}
-          >
-            <li>
-              <h6 className="text-center">會員中心</h6>
-              <p className="text-center">
-                會員姓名: {userName}
-                <br />
-                帳號: {userMail}
-              </p>
-            </li>
-            <li>
-              <hr className="dropdown-divider" />
-            </li>
-            <li>
-              <Link className="dropdown-item text-center" href="/customer">
-                會員管理區
-              </Link>
-            </li>
 
-            <li>
-              <Link className="dropdown-item text-center " href="/about">
-                兌換
-              </Link>
-            </li>
-            <li>
-              <Link
-                href="#/"
-                onClick={handleLogout}
-                className="dropdown-item text-center "
-              >
-                登出
-              </Link>
-            </li>
-          </ul>
-        </li>
-        <li>
-          <Link href={`/product/cart`} className={styles.iconShoppingCartA}>
-            <ShoppingBag size={34} className={styles.iconShoppingCart} />
-          </Link>
-        </li>
-      </ul>
-    </>
+  useEffect(() => {
+    setShowRedDot(cartItems && cartItems.length > 0)
+  }, [cartItems])
+
+  return (
+    <ul className="navbar-nav pe-2 ms-auto d-flex gap-4 align-items-center">
+      <li
+        // className="nav-item dropdown"
+        className={` dropdown ${styles['dropdown']}`}
+      >
+        {auth.userData.id ? (
+          <>
+            <Link
+              className={`dropdown-toggle btn ${styles.DropdownToggle}`}
+              href="/customer"
+              role="button"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+              title="會員中心"
+            >
+              <h5 className={` ${styles.member}`}>
+                <Opensuse size={70} className={styles.iconOpensuse} />
+                會員中心
+              </h5>
+              <p className="d-none d-md-inline d-lg-none">會員中心</p>
+            </Link>
+            <ul
+              className={`dropdown-menu dropdown-menu-end p-4  ${styles['slideIn']} ${styles['dropdown-menu']}`}
+            >
+              <li>
+                <h6 className="text-center">會員中心</h6>
+                <p className="text-center">
+                  會員姓名: {userName}
+                  <br />
+                  帳號: {userMail}
+                </p>
+              </li>
+              <li>
+                <hr className="dropdown-divider" />
+              </li>
+              <li>
+                <Link className="dropdown-item text-center" href="/customer">
+                  會員管理區
+                </Link>
+              </li>
+
+              <li>
+                <Link className="dropdown-item text-center " href="/about">
+                  兌換
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="#/"
+                  onClick={handleLogout}
+                  className="dropdown-item text-center "
+                >
+                  登出
+                </Link>
+              </li>
+            </ul>
+          </>
+        ) : (
+          <li>
+            <Link href="/welcome/login" className={`btnOrangePc transition`}>
+              <div>登入/註冊</div>
+            </Link>
+          </li>
+        )}
+      </li>
+      <li>
+        {/* <button className={`btnOrangePc transition`}>登入/註冊</button> */}
+        <Link href={`/product/cart`} className={styles.iconShoppingCart}>
+          <ShoppingBag size={34} className={styles.iconShoppingCart} />
+          {showRedDot && <div className={styles.redSpot} />}
+          {/* <div className={styles.redSpot}></div> */}
+        </Link>
+      </li>
+    </ul>
   )
 }
