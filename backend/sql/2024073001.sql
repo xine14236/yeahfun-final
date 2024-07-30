048bb6c638afb635c912a5323aaf96851ab94998
Gary <xine9898@gmail.com>
2024-07-30 09:54:34 +0800

    把073001建立資料庫

--- backend/sql/2024073001.sql
+++ backend/sql/2024073001.sql
@@ -21,6 +21,9 @@ SET time_zone = "+00:00";
 -- 資料庫： `project`
 --
 
+CREATE DATABASE IF NOT EXISTS `project` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `project`;
+
 -- --------------------------------------------------------
 
 --
